8b373b6ca6e7a28f234925768f45f0966f116d15
Henrique Marques <hfvmarques@gmail.com>
2021-12-30 16:15:07 -0400

    created initial seed for dev db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  image_url: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/%C3%8Dcone-Bitcoin-PNG.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  image_url: "https://cryptologos.cc/logos/dash-dash-logo.png"
+)
+
+puts "Moedas cadastradas com sucesso!"
