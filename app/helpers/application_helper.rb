ad37208f46fce3ee43f4fa12f54bae4de302797e
Henrique Marques <hfvmarques@gmail.com>
2022-01-04 14:54:30 -0400

    app partially translated with yml file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-  def locale
-    I18n.locale == :en ? "Inglês dos EUA" : "Português do Brasil"
-  end
-  
-
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
