4abee0e39686a980c08c3da26aba740c90a4ea43
Henrique <hfvmarques@gmail.com>
2022-01-08 08:03:01 -0400

    implemented language choosing in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    cookies[:locale] = params[:locale] if params[:locale]
+
+    if cookies[:locale]
+      I18n.locale = cookies[:locale] if I18n.locale != cookies[:locale]          
+    end  
+  end    
 end
